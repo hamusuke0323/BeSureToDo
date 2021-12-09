89253586af4f7cffd7f7b36db5d0cc7d500571e1
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2021-12-09 18:34:25 +0900

    some changes

--- src/main/java/com/hamusuke/criticalib/mixin/LivingEntityMixin.java
+++ src/main/java/com/hamusuke/criticalib/mixin/LivingEntityMixin.java
@@ -4,14 +4,18 @@ import com.hamusuke.criticalib.invoker.LivingEntityInvoker;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.ProjectileDamageSource;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity implements LivingEntityInvoker {
@@ -24,6 +28,13 @@ public abstract class LivingEntityMixin extends Entity implements LivingEntityIn
     @Inject(method = "initDataTracker", at = @At("TAIL"))
     private void initDataTracker(CallbackInfo ci) {
         this.dataTracker.startTracking(CRITICAL, false);
+    }
+
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void damageFirst(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source instanceof ProjectileDamageSource && source.getAttacker() instanceof LivingEntityInvoker livingEntityInvoker && source.getSource() instanceof PersistentProjectileEntity projectile) {
+            livingEntityInvoker.setCritical(projectile.isCritical());
+        }
     }
 
     @Override
